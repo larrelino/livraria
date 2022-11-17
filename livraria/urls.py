e4a868e13159ebe32524727b7cb4cd97bc116649
Lucas Larre Lino <lucas.lino3124@gmail.com>
2022-11-17 16:51:04 -0300

    commit 20

--- livraria/urls.py
+++ livraria/urls.py
@@ -13,24 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
+                                   SpectacularSwaggerView)
 from rest_framework.routers import DefaultRouter
-from core.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
+
+from core.views import (AutorViewSet, CategoriaViewSet, EditoraViewSet,
+                        LivroViewSet)
 from media.router import router as media_router
-
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
-)
-
 
 router = DefaultRouter()
 router.register(r'categorias', CategoriaViewSet)
